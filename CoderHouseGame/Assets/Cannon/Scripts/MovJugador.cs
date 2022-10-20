ca0b1c0ebb2e18b85929699b9d88f83c0e30b4b9
FedexFede <fedebalbo@gmial.com>
2022-10-20 13:41:01 -0300

    V 0.0.4

--- CoderHouseGame/Assets/Cannon/Scripts/MovJugador.cs
+++ CoderHouseGame/Assets/Cannon/Scripts/MovJugador.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class MovJugador : MonoBehaviour
 {
-    
+    public float speed = 20f;
+    public GameObject cam1;
+    public GameObject cam2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.SetActive(true);
+        cam2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleCam();
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+    }
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        transform.position += (move * speed * Time.deltaTime);
+    }
+
+    void ToggleCam()
+    {
+        if (cam1.activeInHierarchy)
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+        else if (cam2.activeInHierarchy)
+        {
+            cam2.SetActive(false);
+            cam1.SetActive(true);
+        }
     }
 }
